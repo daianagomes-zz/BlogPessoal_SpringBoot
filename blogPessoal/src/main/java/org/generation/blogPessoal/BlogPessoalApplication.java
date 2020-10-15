3286f69cb1fa797227d060810ef5bb244b14ff11
unknown <daianagomes@id.uff.br>
2020-10-15 15:27:32 -0300

    Término do CRUD

--- blogPessoal/src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogPessoalApplication {
+public class BlogPessoalApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(BlogPessoalApplication.class, args);
 	}
 
