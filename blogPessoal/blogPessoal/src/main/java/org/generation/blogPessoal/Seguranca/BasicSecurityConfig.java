d3afca2684db9af7bdce9355fb6f2010cfe7111e
unknown <daianagomes@id.uff.br>
2020-10-15 22:08:58 -0300

    Adicionando segurança

---
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/Seguranca/BasicSecurityConfig.java
@@ -0,0 +1,12 @@
+package org.generation.blogPessoal.Seguranca;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter
+{
+	@Autowired
+	private User
+}
---
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -0,0 +1,39 @@
+package org.generation.blogPessoal.model;
+
+//sem anotações
+public class UserLogin 
+{
+
+	
+	
+		private String nome;
+		private String usuario;
+		private String  senha;
+		public String getNome() 
+		{
+			return nome;
+		}
+		public void setNome(String nome) 
+		{
+			this.nome = nome;
+		}
+		public String getUsuario() 
+		{
+			return usuario;
+		}
+		public void setUsuario(String usuario) 
+		{
+			this.usuario = usuario;
+		}
+		public String getSenha() 
+		{
+			return senha;
+		}
+		public void setSenha(String senha) 
+		{
+			this.senha = senha;
+		}
+		
+		
+}
+
